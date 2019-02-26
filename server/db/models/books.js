5362a8091adc60929cbb1abc58ed559571299fed
Sam Bengtson <sambeng1@gmail.com>
2019-02-26 10:11:40 -0600

    added books model

---
+++ server/db/models/books.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Books = db.define('books', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.NUMBER,
+    allowNull: false,
+    validate: {
+      isNumeric: true
+    }
+  },
+  inventoryQuantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  photoUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'http://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png',
+    validate: {
+      isUrl: true
+    }
+  }
+})
+
+module.exports = Books
