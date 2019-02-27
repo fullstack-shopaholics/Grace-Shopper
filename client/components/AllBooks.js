2fbe5a7125084f5404423d153f39d48a25bb0363
Sam Bengtson <sambeng1@gmail.com>
2019-02-27 15:35:55 -0600

    Added search bar
    Added a search bar, changed AllBooks component to a class component, and added local state with the search term on it.

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -4,35 +4,67 @@ import {CardDeck, Card} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import Filters from './Filters'
 
-const AllBooks = props => {
-  const filters = props.filters || []
-  const books = filters.length === 0 ? props.books : props.filterBooks
-  return (
-    <div>
-      <Filters />
-      <CardDeck>
-        {books === undefined || books.length === 0 ? (
-          <li>No Books!</li>
-        ) : (
-          books.map(book => {
-            return (
-              <Link key={book.id} to={`/books/${book.id}`}>
-                <Card style={{width: '250px'}}>
-                  <Card.Img variant="top" src={book.photoUrl} />
-                  <Card.Title>{book.title}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {book.price}
-                  </Card.Subtitle>
-                </Card>
-              </Link>
-            )
-          })
-        )}
-      </CardDeck>
-    </div>
-  )
-}
+class AllBooks extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchTerm: ''
+    }
+    this.changeHandler = this.changeHandler.bind(this)
+  }
 
+  changeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+    console.log(this.state.searchTerm)
+  }
+
+  render() {
+    const filters = this.props.filters || []
+    let books = filters.length === 0 ? this.props.books : this.props.filterBooks
+    if (this.state.searchTerm) {
+      books = books.filter(book => {
+        return (
+          book.title
+            .toLowerCase()
+            .indexOf(this.state.searchTerm.toLowerCase()) !== -1
+        )
+      })
+    }
+    return (
+      <div>
+        <Filters />
+        <input
+          type="search"
+          placeholder="Search Books"
+          value={this.state.searchTerm}
+          name="searchTerm"
+          onChange={this.changeHandler}
+        />
+        <CardDeck>
+          {books === undefined || books.length === 0 ? (
+            <li>No Books!</li>
+          ) : (
+            books.map(book => {
+              return (
+                <Link key={book.id} to={`/books/${book.id}`}>
+                  <Card style={{width: '250px'}}>
+                    <Card.Img variant="top" src={book.photoUrl} />
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {book.price}
+                    </Card.Subtitle>
+                  </Card>
+                </Link>
+              )
+            })
+          )}
+        </CardDeck>
+      </div>
+    )
+  }
+}
 const mapState = state => {
   return {
     books: state.books,
