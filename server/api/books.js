b334ffafb92d730d6fb0a2b808d6f5feac8efd6c
Matti Barzilai <matti.barzilai@gmail.com>
2019-02-27 12:09:08 -0600

    add PUT /api/books/:id

--- server/api/books.js
+++ server/api/books.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Book, Author, Category} = require('../db/models')
+const adminOnly = require('./isAdmin')
 
 module.exports = router
 
@@ -14,7 +15,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', adminOnly, async (req, res, next) => {
   try {
     const {
       title,
@@ -54,6 +55,56 @@ router.post('/', async (req, res, next) => {
       include: [{model: Author}, {model: Category}]
     })
     res.json(newBook)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', adminOnly, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const book = await Book.findById(id)
+    const {
+      title,
+      description,
+      price,
+      inventoryQuantity,
+      photoUrl,
+      author,
+      categories
+    } = req.body
+    const [authorInstance] = await Author.findOrCreate({
+      where: {
+        name: author
+      }
+    })
+    const bookCategories = await categories.reduce(async function(
+      allCats,
+      category
+    ) {
+      allCats = await allCats
+      const categoryInstance = await Category.findOne({where: {name: category}})
+      allCats.push(categoryInstance)
+      return allCats
+    },
+    [])
+
+    let updatedBook = await book.update({
+      title,
+      description,
+      price,
+      inventoryQuantity,
+      photoUrl
+    })
+
+    await updatedBook.setAuthor(authorInstance)
+    await updatedBook.setCategories(bookCategories)
+
+    updatedBook = await Book.findById(updatedBook.id, {
+      include: [{model: Author}, {model: Category}]
+    })
+
+    res.json(updatedBook)
   } catch (err) {
     next(err)
   }
