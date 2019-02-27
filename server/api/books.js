376759a83ba2ccfe1dfca1dd68074e74d29efa41
jpoll15 <jpollack015@gmail.com>
2019-02-27 11:31:21 -0600

    successful POST /api/books

--- server/api/books.js
+++ server/api/books.js
@@ -13,3 +13,48 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {
+      title,
+      description,
+      price,
+      inventoryQuantity,
+      photoUrl,
+      author,
+      categories
+    } = req.body
+
+    const [authorInstance] = await Author.findOrCreate({
+      where: {
+        name: author
+      }
+    })
+    const bookCategories = await categories.reduce(async function(
+      allCats,
+      category
+    ) {
+      allCats = await allCats
+      const categoryInstance = await Category.findOne({where: {name: category}})
+      allCats.push(categoryInstance)
+      return allCats
+    },
+    [])
+    let newBook = await Book.create({
+      title,
+      description,
+      price,
+      inventoryQuantity,
+      photoUrl
+    })
+    await newBook.setAuthor(authorInstance)
+    await newBook.addCategories(bookCategories)
+    newBook = await Book.findById(newBook.id, {
+      include: [{model: Author}, {model: Category}]
+    })
+    res.json(newBook)
+  } catch (err) {
+    next(err)
+  }
+})
